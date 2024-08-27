fc3cf020cb4dfb5e1c604613a61378e8df08f2a7
Anamul Haque <anamulhaque9901@gmail.com>
2024-08-27 17:23:33 +0600

    function update of bookin gpage

--- src/pages/Booking/BookingPage.tsx
+++ src/pages/Booking/BookingPage.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 
 const BookingPage = () => {
   const [bookingData, setBookingData] = useState();
+  const [selectDate, setSelectDate] = useState("");
   const { id } = useParams();
   const { data: allFacility } = useGetAllFacilityQuery(undefined);
   const { data: availableBooking } = useCheckAvailablBookingQuery(bookingData, {
@@ -20,6 +21,7 @@ const BookingPage = () => {
     const data = form.date.value;
     const checkBookingData = { date: data, facilityId: id };
     setBookingData(checkBookingData as any);
+    setSelectDate(data);
   };
 
   // ------------------------
@@ -52,13 +54,15 @@ const BookingPage = () => {
     const form = e.target;
     const startTime = form.startTime.value;
     const endTime = form.endTime.value;
+    const facility = id;
+    const date = selectDate;
 
-    const bookingData = { startTime, endTime };
+    const bookingData = { startTime, endTime, facility, date };
     console.log(bookingData);
   };
   return (
-    <div className="min-h-[100vh] max-w-7xl mx-auto w-full">
-      <div>
+    <div className="min-h-[100vh] pt-9 max-w-7xl mx-auto w-full px-4 md:px-0">
+      <div className="pb-2">
         <h1 className="text-2xl font-bold text-center">Booking page</h1>
       </div>
       <div className="">
@@ -67,7 +71,7 @@ const BookingPage = () => {
           ?.filter((item: any) => item?._id == id)
           ?.map((item: any) => (
             <div key={item?._id} className="flex justify-center pt-8">
-              <div className="bg-slate-200 w-1/2 p-2">
+              <div className="bg-slate-200 w-full md:w-1/2 p-2">
                 <h1 className="text-xl font-semibold">{item?.name}</h1>
                 <div className="flex gap-5">
                   <h1>Prce: {item?.pricePerHour}$</h1>
@@ -80,27 +84,31 @@ const BookingPage = () => {
         <form onSubmit={handleCheckBooking} action="">
           <div className="flex justify-center pt-2">
             {/* date picker */}
-            <div className=" w-1/2  flex justify-between">
+            <div className=" w-full md:w-1/2 gap-3 flex justify-between">
               <input
                 required
-                className="border px-8"
+                className="border w-full"
                 type="date"
                 name="date"
                 id=""
               />
-              <button className="btn btn-sm rounded-none w-1/2">Submin</button>
+              <div className="w-full">
+                <button className="btn btn-sm rounded-none w-full">
+                  Submin
+                </button>
+              </div>
             </div>
           </div>
         </form>
         {/* End form */}
 
         {/* start avilabile time div  */}
-        <div className="pt-6">
+        <div className="pt-6 pb-8">
           <h1 className="text-center text-xl font-semibold pb-2">
             Available Slots
           </h1>
           <div className="flex justify-center ">
-            <div className=" w-1/2 grid grid-cols-2 justify-between gap-3 items-center">
+            <div className="w-full md:w-1/2 grid grid-cols-2 justify-between gap-3 items-center">
               {mergedData?.map((item: any, idx: number) => (
                 <div key={idx} className="w-full">
                   <div className="bg-slate-300 text-center py-1">
@@ -113,8 +121,8 @@ const BookingPage = () => {
             </div>
           </div>
           {mergedData.length == 0 && (
-            <h1 className="text-center border w-1/2 mx-auto">
-              Slot not available
+            <h1 className="text-center border w-full md:w-1/2 mx-auto">
+              No slot available
             </h1>
           )}
         </div>
@@ -123,7 +131,7 @@ const BookingPage = () => {
         {/* start book time div  */}
         <form onSubmit={handleBooking} action="">
           <div className="flex justify-center pt-6">
-            <div className=" w-1/2 flex justify-between gap-8 items-center">
+            <div className="w-full md:w-1/2 flex justify-between gap-8 items-center">
               <div className="w-full">
                 <h1>Start Time</h1>
                 <div className=" border text-center">
@@ -148,8 +156,8 @@ const BookingPage = () => {
               </div>
             </div>
           </div>
-          <div className="text-center pt-6 w-1/2 mx-auto">
-            <button className="btn btn-sm w-full rounded-none">submit</button>
+          <div className="text-center pt-6 w-full md:w-1/2 mx-auto">
+            <button disabled={!selectDate} className="btn btn-sm w-full rounded-none">submit</button>
           </div>
         </form>
         {/* end book time div  */}
